f728dc5281e532a17a1b9a289a8a66519119e585
pant2002 <pant.aniket2002123@gmail.com>
2015-10-04 21:31:11 -0500

    Finished Number Counter App
    Finished Number Counter App

---
+++ NumberCounter/NumberCounter/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  NumberCounter
+//
+//  Created by Dev/Github on 10/4/15.
+//  Copyright © 2015 Sterna-Airlines. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+-(IBAction)Up:(id)sender{
+    NumberCount = NumberCount + 1;
+    CounterDisplay.text = [NSString stringWithFormat:@"%i", NumberCount];
+}
+-(IBAction)Down:(id)sender{
+    NumberCount = NumberCount - 1;
+    CounterDisplay.text = [NSString stringWithFormat:@"%i", NumberCount];
+}
+-(IBAction)Restart:(id)sender{
+    NumberCount = 0;
+    CounterDisplay.text = [NSString stringWithFormat:@"%i", NumberCount];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
