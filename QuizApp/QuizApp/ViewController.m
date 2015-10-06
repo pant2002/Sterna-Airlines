76c03993e512529022c342a7e8b3bd1da84baa2f
Sybersid <sidhdharth.suresh@gmail.com>
2015-10-06 11:16:53 -0500

    QuizApp

---
+++ QuizApp/QuizApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QuizApp
+//
+//  Created by Sid Suresh on 10/6/15.
+//  Copyright © 2015 Sid Suresh and Aniket Pant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
